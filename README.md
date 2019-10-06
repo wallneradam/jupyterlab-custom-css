7f93b2b453390490b398bc36af7acf3f93e2ae53
wallneradam <adam.wallner@gmail.com>
2019-10-06 19:05:18 +0200

    Initial comit

---
+++ README.md
@@ -0,0 +1,53 @@
+# custom-css
+
+Add custom CSS rules for JupyterLab
+
+## Usage
+
+After install the plugin, you should see an item called "Custom CSS" in the settings in "Advanced Settings Editor".
+Here you can specify any CSS rules you want.
+
+E.g. if you want to change the maximum output size in scrolled mode:
+```json
+{
+    "rules": [
+        {
+            "selector": ".jp-CodeCell.jp-mod-outputsScrolled .jp-Cell-outputArea",
+            "styles": [
+                "max-height: 300px;"
+            ]
+        }
+    ]
+}
+```
+
+This will create an inline `<style>` element in the document`s body.
+
+When you save changes it will immediately be active, so you don't need to restart JupyterLab.
+
+## Prerequisites
+
+* JupyterLab
+
+## Installation
+
+```bash
+jupyter labextension install @wallneradam/custom_css
+```
+
+## Development
+
+For a development install (requires npm version 4 or later), do the following in the repository directory:
+
+```bash
+npm install
+npm run build
+jupyter labextension link .
+```
+
+To rebuild the package and the JupyterLab app:
+
+```bash
+npm run build
+jupyter lab build
+```
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+const PLUGIN_NAME = "@wallneradam/custom_css";
+
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { ISettingRegistry } from '@jupyterlab/coreutils';
+import { JSONObject } from '@phosphor/coreutils';
+
+
+interface ICSSRule extends JSONObject {
+    selector: string;
+    styles: string[];
+}
+
+
+const extension: JupyterFrontEndPlugin<void> = {
+    id: PLUGIN_NAME,
+    requires: [ISettingRegistry],
+    autoStart: true,
+
+    activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry) => {
+        console.log('JupyterLab extension custom_css is activated!');
+
+        let styleElement = document.createElement("style");
+        styleElement.type = "text/css";
+        styleElement.id = PLUGIN_NAME;
+        document.body.appendChild(styleElement);
+
+        /**
+         * Update settings on change
+         * @param settings The loaded settings
+         */
+        function updateOptions(settings: ISettingRegistry.ISettings): void {
+            let styles = "";
+            let rules = settings.composite.rules as ICSSRule[];
+            for (let rule of rules) {
+                styles += `${rule.selector} \{`;
+                styles += "\n  " + rule.styles.join("\n  ")
+                styles += "\n}";
+            }
+            document.body.removeChild(styleElement);
+            styleElement.innerHTML = styles;
+            document.body.appendChild(styleElement);
+        }
+
+        // Load settings
+        settingRegistry
+            .load(extension.id + ':plugin')
+            .then(settings => {
+                // Update options
+                updateOptions(settings);
+
+                // Update options on settings changed
+                settings.changed.connect(() => {
+                    updateOptions(settings);
+                });
+            });
+    }
+};
+
+
+export default extension;
